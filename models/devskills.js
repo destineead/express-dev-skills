2a90207bb60bf5b99cc0883c12c973544a84e2b4
destineead <destineenugent1@gmail.com>
2024-01-03 17:23:46 -0500

    Devskills Refractor - Routing

--- models/devskills.js
+++ models/devskills.js
@@ -1,5 +1,13 @@
-// const devskills = [
-   // {id: 125223, devskill: 'Feed Dogs', done: true},
-   // {id: 127904, devskill: 'Learn Express', done: false},
-   // {id: 139608, devskill: 'Buy Milk', done: false}
- // ];
+const devskills = [
+    {id: 125223, devskill: 'Javascript', done: true},
+    {id: 127904, devskill: 'HTML', done: true},
+    {id: 139608, devskill: 'CSS', done: true},
+  ];
+	
+  module.exports = {
+    getAll
+  };
+	
+  function getAll() {
+    return devskills;
+  }

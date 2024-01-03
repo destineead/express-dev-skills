c914f288f60cc7e4586841965be67a4a62491534
destineead <destineenugent1@gmail.com>
2024-01-03 16:46:04 -0500

    Create initial files

---
+++ models/devskills.js
@@ -0,0 +1,5 @@
+// const devskills = [
+   // {id: 125223, devskill: 'Feed Dogs', done: true},
+   // {id: 127904, devskill: 'Learn Express', done: false},
+   // {id: 139608, devskill: 'Buy Milk', done: false}
+ // ];
